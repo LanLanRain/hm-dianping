483f63097efc06d0e852f1b5466d6f2d62e768fe
LunerBreeze <RainSoul1120@outlook.com>
2024-09-03 23:51:16 +0800

    解决Redis分布式锁误删问题

--- src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,5 +1,6 @@
 package com.hmdp.utils;
 
+import cn.hutool.core.lang.UUID;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.util.concurrent.TimeUnit;
@@ -8,7 +9,7 @@ import java.util.concurrent.TimeUnit;
  * @author RainSoul
  * @create 2024-09-03
  */
-public class SimpleRedisLock implements ILock{
+public class SimpleRedisLock implements ILock {
 
     private String name;
     private StringRedisTemplate stringRedisTemplate;
@@ -20,6 +21,8 @@ public class SimpleRedisLock implements ILock{
 
     private static final String KEY_PREFIX = "lock:";
 
+    private static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
+
     /**
      * 尝试获取分布式锁
      *
@@ -29,7 +32,7 @@ public class SimpleRedisLock implements ILock{
     @Override
     public boolean tryLock(Long timeoutSec) {
         // 获取当前线程的ID，并将其转换为字符串格式，用于标识锁的持有者
-        String threadId = Thread.currentThread().getId() + "";
+        String threadId = ID_PREFIX + Thread.currentThread().getId() + "";
         // 尝试使用Redis的setIfAbsent方法来获取锁
         // 如果键不存在，则设置键值对，表示成功获取锁
         // 键名由KEY_PREFIX和name拼接而成，确保锁的唯一性
@@ -42,6 +45,14 @@ public class SimpleRedisLock implements ILock{
 
     @Override
     public void unlock() {
-        stringRedisTemplate.delete(KEY_PREFIX + name);
+        // 获取线程标示
+        String threadId = ID_PREFIX + Thread.currentThread().getId();
+        // 获取锁中的标示
+        String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
+        // 判断标示是否一致
+        if (threadId.equals(id)) {
+            // 释放锁
+            stringRedisTemplate.delete(KEY_PREFIX + name);
+        }
     }
 }
